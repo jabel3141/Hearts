5eb9e68866f29c6cf5bf6e8c13e54f9e2549a67f
muggleman1 <31550810+muggleman1@users.noreply.github.com>
2020-05-03 19:51:07 -0400

    Bugfix + fix save location

--- play_nn.py
+++ play_nn.py
@@ -116,7 +116,7 @@ class play_nn():
             if my_hand.hasCard(a_card):
                 nn_input[i * 2] = 1
             elif a_card in trick.trick:
-                nn_input[1 * 2 + 2] = 1
+                nn_input[i * 2 + 2] = 1
             elif player_1.has_played(a_card) or player_2.has_played(a_card) or player_3.has_played(a_card)\
                     or me.has_played(a_card):
                 nn_input[i * 2 + 1] = 1
